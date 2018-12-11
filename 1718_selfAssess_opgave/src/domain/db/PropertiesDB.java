74f497fdd2a4e8cdc3efa7cd6fd1d057ac8c0642
Arne Vandoorslaer <arne.vandoorslaer@student.ucll.be>
2018-12-11 20:01:45 +0100

    propertiesDB heeft een read en write functie

--- 1718_selfAssess_opgave/src/domain/db/PropertiesDB.java
+++ 1718_selfAssess_opgave/src/domain/db/PropertiesDB.java
@@ -13,7 +13,11 @@ public class PropertiesDB {
         return  this.prop;
     }
     public PropertiesDB(){
-/*        try {
+        read();
+    }
+
+    public void write(){
+        /*        try {
             output = new FileOutputStream("evaluation.properties");
             // set the properties value
             prop.setProperty("evaluation.mode", "feedback");
@@ -32,7 +36,9 @@ public class PropertiesDB {
             }
 
         }*/
+    }
 
+    public void read(){
         try {
             this.input = new FileInputStream("evaluation.properties");
             System.out.println(this.input);
@@ -54,6 +60,5 @@ public class PropertiesDB {
                 }
             }
         }
-
     }
 }
